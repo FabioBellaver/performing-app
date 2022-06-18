8eaaaeeb68e64df92e59e7658632fdb12af05dbd
Fábio Bellaver <fabio.bellaver658@gmail.com>
2022-06-18 17:14:49 -0300

    using react 'memo'

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 interface ProductItemProps {
   product: {
     id: number;
@@ -6,10 +8,25 @@ interface ProductItemProps {
   };
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   );
 }
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product);
+    /*
+    'memo' avoid the component to enter in the default renderization flux
+    use case of 'memo'
+    1 - Pure Functional Components ();
+    2 - Renders too often;
+    3 - Re-render with same props;
+    4 - Medium to big size.
+    */
+  }
+);

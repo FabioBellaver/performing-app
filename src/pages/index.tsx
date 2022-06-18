28f7bf8bb303649375b1a53603b85467cfe43119
Fábio Bellaver <fabio.bellaver658@gmail.com>
2022-06-18 16:35:00 -0300

    first-commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+import type { NextPage } from "next";
+import { FormEvent, useState } from "react";
+import { SearchResults } from "../components/SearchResults";
+
+const Home: NextPage = () => {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  const handleSearch = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const data = await response.json();
+
+    setResults(data);
+  };
+
+  return (
+    <div>
+      <h1>Search</h1>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      <SearchResults results={results}/>
+
+    </div>
+  );
+};
+
+export default Home;
